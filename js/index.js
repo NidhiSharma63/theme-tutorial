2e49831c129bc91e96dd137d928f541bf7bec457
NidhiSharma63 <nidhisharma639593@gmail.com>
2022-05-24 01:17:38 +0100

    added:theme circle

--- js/index.js
+++ js/index.js
@@ -1,10 +1,16 @@
 import {
     addHoverEffect
 } from './btns.style.js';
+
 import {
     getValue,
     Action
-} from './get-value.js'
+} from './get-value.js';
+
+import{
+    moveCircle,
+} from './theme.js';
+
 const btns = document.querySelectorAll('.btn');
 const result = document.querySelector(".result");
 const onBtn = document.getElementById("on-btn");
@@ -29,3 +35,5 @@ equal.addEventListener('click', () => {
         Action()
     }
 });
+
+moveCircle();
---
+++ js/theme.js
@@ -0,0 +1,4 @@
+
+const moveCircle = (circle) =>{
+
+}
