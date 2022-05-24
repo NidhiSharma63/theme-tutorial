e4a87d671f9675b3bf60422bc3e8eeba8d1224fc
NidhiSharma63 <nidhisharma639593@gmail.com>
2022-05-24 02:59:04 +0100

    local storage added

--- js/theme.js
+++ js/theme.js
@@ -1,16 +1,49 @@
 
 export const moveCircle = (circle,themeLink) =>{
+    console.log(localStorage.getItem('theme-href'));
+    let themeStage =1;
+    if(localStorage.getItem('theme-href')===null){
+        themeLink.setAttribute('href','./theme.css/dark-blue-theme.css');
+        circle.style.transform+='translateX(0px)';
+    }
+    else{
+        const preSelectedTheme = localStorage.getItem('theme-href');
+        themeLink.setAttribute('href',preSelectedTheme);
+    }
+
+    // fixing circle position to According to theme
+    if(localStorage.getItem('theme-href')==='./theme.css/light-gray-theme.css'){
+        circle.style.transform+='translateX(30px)';
+    }
+    if(localStorage.getItem('theme-href')==='./theme.css/dark-voilet.css'){
+        circle.style.transform+='translateX(60px)';
+    }
+
+    //circle event
+
     circle.addEventListener('click', () => {
         const val = circle.getBoundingClientRect().x;
-        if(val>=650){
-            circle.style.transform+='translateX(-60px)';
-            themeLink.setAttribute('href','./theme.css/dark-blue-theme.css');
-        }else{
-            circle.style.transform+='translateX(30px)';
-            themeLink.setAttribute('href','./theme.css/light-gray-theme.css');
-        }
-        if(val>626 && val<650){
-            themeLink.setAttribute('href','./theme.css/dark-voilet.css')
-        }
+        themeStage++;
+            if(themeStage==1){
+                circle.style.transform+='translateX(-60px)';
+                themeLink.setAttribute('href','./theme.css/dark-blue-theme.css');
+                localStorage.setItem('theme-href','./theme.css/dark-blue-theme.css');
+            }
+
+            if(themeStage==2){
+                circle.style.transform='translateX(30px)'
+                localStorage.setItem('theme-href','./theme.css/light-gray-theme.css');
+                themeLink.setAttribute('href','./theme.css/light-gray-theme.css');
+            }
+
+            if(themeStage==3){
+                circle.style.transform='translateX(60px)'
+                themeLink.setAttribute('href','./theme.css/dark-voilet.css');
+                localStorage.setItem('theme-href','./theme.css/dark-voilet.css');
+                themeStage=0;
+            }
+            console.log(themeStage)
+
+            console.log(localStorage.getItem('theme-href'))
     });
-}
+};
