919104d6b6926f8138ae50ceb88496e5a27fdc54
NidhiSharma63 <nidhisharma639593@gmail.com>
2022-05-24 02:07:32 +0100

    padding added to theme circle

--- js/theme.js
+++ js/theme.js
@@ -3,10 +3,10 @@ export const moveCircle = (circle,themeLink) =>{
     circle.addEventListener('click', () => {
         const val = circle.getBoundingClientRect().x;
         if(val>=650){
-            circle.style.transform+='translateX(-40px)';
+            circle.style.transform+='translateX(-60px)';
             themeLink.setAttribute('href','./theme.css/dark-blue-theme.css');
         }else{
-            circle.style.transform+='translateX(20px)';
+            circle.style.transform+='translateX(30px)';
             themeLink.setAttribute('href','./theme.css/light-gray-theme.css');
         }
         if(val>626 && val<650){
