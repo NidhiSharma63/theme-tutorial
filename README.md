c0ed56c0b6aed65b72e70ea08c13862871f22fa7
NidhiSharma63 <nidhisharma639593@gmail.com>
2022-10-17 16:33:58 +0530

    restore deleted files

---
+++ README.md
@@ -0,0 +1,23 @@
+# Calculator app using-js
+
+![Design preview for the Calculator app coding challenge](./design/desktop-preview.jpg)
+
+## hey Welcome! 👋
+
+**i have completed this challenge given by front-end mentor.**
+
+## About The challenge
+
+- this challenge is bascially to building a Calculator App.
+- In this App user should be able to choose 3 different theme.
+- Prefer-user-theme should be saved in the browser.
+- completely Resposnive.
+
+**i learned lots of new thing while building this challenge**
+**I have done this project by using HTML , CSS , JS chekout this https://nidhisharma63.github.io/calculator-app/**
+
+
+
+
+
+
---
+++ js/theme.js
@@ -0,0 +1,46 @@
+
+export const moveCircle = (circle,themeLink) =>{
+    console.log(localStorage.getItem('theme-href'));
+    let themeStage =1;
+    if(localStorage.getItem('theme-href')===null){
+        themeLink.setAttribute('href','./theme.css/dark-blue-theme.css');
+        circle.style.transform+='translateX(0px)';
+    }
+    else{
+        const preSelectedTheme = localStorage.getItem('theme-href');
+        themeLink.setAttribute('href',preSelectedTheme);
+    }
+
+    // fixing circle position to According to theme
+    if(localStorage.getItem('theme-href')==='./theme.css/light-gray-theme.css'){
+        circle.style.transform+='translateX(30px)';
+    }
+    if(localStorage.getItem('theme-href')==='./theme.css/dark-voilet.css'){
+        circle.style.transform+='translateX(60px)';
+    }
+
+    //circle event
+
+    circle.addEventListener('click', () => {
+        const val = circle.getBoundingClientRect().x;
+        themeStage++;
+            if(themeStage==1){
+                circle.style.transform+='translateX(-60px)';
+                themeLink.setAttribute('href','./theme.css/dark-blue-theme.css');
+                localStorage.setItem('theme-href','./theme.css/dark-blue-theme.css');
+            }
+
+            if(themeStage==2){
+                circle.style.transform='translateX(30px)'
+                localStorage.setItem('theme-href','./theme.css/light-gray-theme.css');
+                themeLink.setAttribute('href','./theme.css/light-gray-theme.css');
+            }
+
+            if(themeStage==3){
+                circle.style.transform='translateX(60px)'
+                themeLink.setAttribute('href','./theme.css/dark-voilet.css');
+                localStorage.setItem('theme-href','./theme.css/dark-voilet.css');
+                themeStage=0;
+            };
+    });
+};
